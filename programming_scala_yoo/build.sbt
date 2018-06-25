987228c3cefbca6603cef9a6e7eafdb09f355230
samnigth <wlgnfl100@gmail.com>
2018-06-26 00:00:00 +0900

    revert

--- programming_scala_yoo/build.sbt
+++
@@ -1,7 +0,0 @@
-name := "programming_scala_yoo"
-
-version := "0.1"
-
-organization := "com.samnight.study"
-
-scalaVersion := "2.12.6"
--- programming_scala_yoo/src/main/scala/com/samnight/function/sampleFn.scala
+++
@@ -1,9 +0,0 @@
-package com.samnight.function
-
-object sampleFn {
-
-  def captials(str: String): String ={
-    var captialMap = Map("US" -> "Washington", "France" -> "Paris", "Japan" -> "Tokyo")
-    captialMap(str)
-  }
-}
